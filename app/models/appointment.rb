75d8ea44fc894d2e6fb617777d1890e1a8e0b110
Mike Gregory <mgregory8219@gmail.com>
2016-06-03 02:53:52 -0400

    some refactoring on appointment.rb, needs much more

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -11,47 +11,100 @@ class Appointment < ActiveRecord::Base
   end
 
   def set_date_search_variables(appt_params)
-    # Set Defaults
+
+    @search_spec = get_date_search_specificity(appt_params)
+
+    set_beg_times_initial
+    replace_beg_times_with_params(appt_params)
+
+    set_end_times_initial
+    adjust_times_on_search_spec
+
+    set_beg_end_dates
+
+    appt_params[:start_time] = @date_range
+    appt_params[:end_time] = @date_range
+    appt_params.delete(:hour) if !appt_params[:hour].nil?
+    appt_params.delete(:day) if !appt_params[:day].nil?
+    appt_params.delete(:month) if !appt_params[:month].nil?
+    appt_params.delete(:year) if !appt_params[:year].nil?
+    appt_params
+  end
+
+  def set_beg_times_initial
     now = DateTime.now
-    beg_hour = now.hour
-    end_hour = now.hour + 1.hours
-    beg_day = now.day
-    end_day = now.day + 1.days
-    beg_month = now.month
-    end_month = now.month + 1.months
-    beg_year = now.year
-    end_year = now.year + 1.years
-    # Weeks don't work in combination with hour, day, month, year
-    # beg_week = now.beginning_of_week
-    # end_week = now.end_of_week
-    # add weeks search option if time is available
+    @beg_hour = now.hour
+    @beg_month = now.month
+    @beg_day = now.day
+    @beg_year = now.year
+  end
 
-    # Replace with params (if given)
-    beg_hour = appt_params[:hour] if appt_params[:hour]
-    end_hour = (appt_params[:hour] + 1.hours) if appt_params[:hour]
-    beg_day = appt_params[:day] if appt_params[:day]
-    end_day = (appt_params[:day] + 1.days) if appt_params[:day]
-    beg_month = appt_params[:month] if appt_params[:month]
-    end_month = (appt_params[:month] + 1.months) if appt_params[:month]
-    beg_year = appt_params[:year] if appt_params[:year]
-    end_year = (appt_params[:year] + 1.years) if appt_params[:year]
+  def replace_beg_times_with_params(appt_params)
+    @beg_hour = appt_params[:hour].to_i if appt_params[:hour]
+    @beg_day = appt_params[:day].to_i if appt_params[:day]
+    @beg_month = appt_params[:month].to_i if appt_params[:month]
+    @beg_year = appt_params[:year].to_i if appt_params[:year]
+  end
 
-    # Create new datetime object
-    beg_date = DateTime.new(beg_year, beg_month, beg_day, beg_hour)
-    end_date = DateTime.new(end_year, end_month, end_day, end_hour)
-    new_params = appt_params
-    appt_params[:start_time] = date
-    appt_params[:end_time] = date
-    appt_params.delete(:hour) if !new_params[:hour].nil?
-    appt_params.delete(:day) if !new_params[:day].nil?
-    appt_params.delete(:month) if !new_params[:month].nil?
-    appt_params.delete(:year) if !new_params[:year].nil?
-    p "*" * 50
-    p beg_date
-    p end_date
-    p appt_params
-    p "*" * 50
-    new_params
+  def set_end_times_initial
+    @end_hour = @beg_hour
+    @end_day = @beg_day
+    @end_month = @beg_month
+    @end_year = @beg_year
+  end
+
+  def adjust_times_on_search_spec
+    if @search_spec == :hour
+      if @beg_hour == 23
+        @end_hour = 0
+      else
+        @end_hour = @beg_hour + 1
+      end
+    elsif @search_spec == :day
+      if @beg_day == Time.days_in_month(@beg_month, @beg_year)
+        @end_day = 1
+      else
+        @end_day = @beg_day + 1
+      end
+      @beg_hour = 0
+      @end_hour = 0
+    elsif @search_spec == :month
+      if @beg_month == 12
+        @end_month = 1
+      else
+        @end_month = @beg_month + 1
+      end
+      @beg_hour = 0
+      @end_hour = 0
+      @beg_day = 1
+      @end_day = 1
+    elsif @search_spec == :year
+      @beg_hour = 0
+      @end_hour = 0
+      @beg_day = 1
+      @end_day = 1
+      @beg_month = 1
+      @end_month = 1
+      @end_year = @beg_year + 1
+    end
+  end
+
+  def set_beg_end_dates
+    @beg_date = DateTime.new(@beg_year, @beg_month, @beg_day, @beg_hour)
+    @end_date = DateTime.new(@end_year, @end_month, @end_day, @end_hour)
+    @date_range = @beg_date..@end_date
+  end
+
+  def get_date_search_specificity(params)
+    if params[:hour]
+      return :hour
+    elsif params[:day]
+      return :day
+    elsif params[:month]
+      return :month
+    elsif params[:year]
+      return :year
+    end
   end
 
 end
