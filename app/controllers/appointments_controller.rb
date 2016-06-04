ed90151a708d1f44de6d1d6f870dd8ca05e32e61
Mike Gregory <mgregory8219@gmail.com>
2016-06-03 22:22:42 -0400

    decided on how to manage user input for which method to call. Also got listing and deleting working within new parameters interface

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -2,6 +2,19 @@ class AppointmentsController < ApplicationController
   skip_before_filter :verify_authenticity_token
   # Worried about security of this line!
 
+  def crud_master
+    case crud_params[:m]
+    when "create"
+      create
+    when "update"
+      update
+    when "delete"
+      destroy
+    else
+      list
+    end
+  end
+
   def list
     if appt_params == {}
       @appointments = Appointment.all
@@ -49,27 +62,55 @@ class AppointmentsController < ApplicationController
   end
 
   def destroy
-    @appointment = Appointment.find(params[:id])
+    if appt_params == {}
+      @appointments = Appointment.all
+    else
+      @appointments = Appointment.where(Appointment.new.set_date_search_variables(appt_params))
+      if @appointments.length == 1
+        @appointment = @appointments.first
+      elsif @appointments.length > 1
+        render status: 422, json: {
+          message: "Delete failed: More than one result"
+        }.to_json
+        return @appointments
+      elsif @appointments.length < 1
+        render status: 422, json: {
+          message: "Delete failed: No result found"
+        }.to_json
+        return @appointments
+      end
+    end
+    p "#{@appointment} deleted"
     @appointment.destroy
     render status: 200, json: {
-      message: "Successfully deleted Appointment"
+      message: "Successfully deleted Appointment",
+      appointment: @appointment
     }.to_json
   end
 
   private
   def appt_params
     params.permit(:first_name, :last_name, :start_time, :end_time, :comments,
-                  :hour, :day, :month, :year, :week)
+                  :min, :hour, :day, :month, :year)
+  end
+
+  def crud_params
+    params.permit(:m)
+  end
+
+  def create_params
+    params.permit(:first_name, :last_name, :start_time, :end_time, :comments)
   end
 
   def reformat_params_date
     @create_success = true
-    new_params = appt_params
+    new_params = validate_times
     begin
       new_params[:start_time] = DateTime
         .strptime(new_params[:start_time], '%m/%d/%Y %H:%M') + 2000.years
       new_params[:end_time] = DateTime
         .strptime(new_params[:end_time], '%m/%d/%Y %H:%M') + 2000.years
+      return new_params
     rescue
       @create_success = false
       error = "One of your dates is invalid."
--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -10,13 +10,7 @@ class Appointment < ActiveRecord::Base
     DateTime.strptime(self.start_time, '%m/%d/%Y %H:%M')
   end
 
-### Problem here where first_name search, start_date search do not work.
-### At least in that first case, it DOES work when some dates are included.
-### Also displays empty JSON when no criteria are given
-### Work on this!
-
   def set_date_search_variables(appt_params)
-
     # Determine most specific time search criteria
     @search_spec = get_date_search_specificity(appt_params)
 
@@ -37,11 +31,14 @@ class Appointment < ActiveRecord::Base
     p "appt_params: #{appt_params}"
     p "*" * 50
     # Pushes date_range back into appt_params if params were given
-    if(appt_params[:hour] ||
-       appt_params[:day] ||
-       appt_params[:month] ||
-       appt_params[:year])
+
+    if(appt_params[:min] ||
+      appt_params[:hour] ||
+      appt_params[:day] ||
+      appt_params[:month] ||
+      appt_params[:year])
       appt_params[:start_time] = @date_range
+      # appt_params[:end_time] = @date_range
     end
     # Removes params not in database and returns appt_params
     delete_unsearchable_params(appt_params)
@@ -50,6 +47,7 @@ class Appointment < ActiveRecord::Base
 
   def set_beg_times_initial
     now = DateTime.now
+    @beg_min = now.min
     @beg_hour = now.hour
     @beg_month = now.month
     @beg_day = now.day
@@ -57,6 +55,7 @@ class Appointment < ActiveRecord::Base
   end
 
   def replace_beg_times_with_params(appt_params)
+    @beg_min = appt_params[:min].to_i if appt_params[:min]
     @beg_hour = appt_params[:hour].to_i if appt_params[:hour]
     @beg_day = appt_params[:day].to_i if appt_params[:day]
     @beg_month = appt_params[:month].to_i if appt_params[:month]
@@ -64,6 +63,7 @@ class Appointment < ActiveRecord::Base
   end
 
   def set_end_times_initial
+    @end_min = @beg_min
     @end_hour = @beg_hour
     @end_day = @beg_day
     @end_month = @beg_month
@@ -72,12 +72,20 @@ class Appointment < ActiveRecord::Base
 
 ### This wordiness is absolutely terrible, refactor before submission, Mike
   def adjust_times_on_search_spec
-    if @search_spec == :hour
+    if @search_spec == :min
+      if @beg_min == 59
+        @end_min = 0
+      else
+        @end_min = @beg_min + 1
+      end
+    elsif @search_spec == :hour
       if @beg_hour == 23
         @end_hour = 0
       else
         @end_hour = @beg_hour + 1
       end
+      @beg_min = 0
+      @end_min = 0
     elsif @search_spec == :day
       if @beg_day == Time.days_in_month(@beg_month, @beg_year)
         @end_day = 1
@@ -86,17 +94,23 @@ class Appointment < ActiveRecord::Base
       end
       @beg_hour = 0
       @end_hour = 0
+      @beg_min = 0
+      @end_min = 0
     elsif @search_spec == :month
       if @beg_month == 12
         @end_month = 1
       else
         @end_month = @beg_month + 1
       end
+      @beg_min = 0
+      @end_min = 0
       @beg_hour = 0
       @end_hour = 0
       @beg_day = 1
       @end_day = 1
     elsif @search_spec == :year
+      @beg_min = 0
+      @end_min = 0
       @beg_hour = 0
       @end_hour = 0
       @beg_day = 1
@@ -105,25 +119,28 @@ class Appointment < ActiveRecord::Base
       @end_month = 1
       @end_year = @beg_year + 1
     else
-      if @beg_day == Time.days_in_month(@beg_month, @beg_year)
-        @end_day = 1
-        @end_month = @end_month + 1
-      else
-        @end_day = @beg_day + 1
-      end
+      # In case today is the last day of the month
+      # if @beg_day == Time.days_in_month(@beg_month, @beg_year)
+      #   @end_day = 1
+      #   @end_month = @end_month + 1
+      # else
+      #   @end_day = @beg_day + 1
+      # end
       @beg_hour = 0
       @end_hour = 0
     end
   end
 
   def set_beg_end_dates
-    @beg_date = DateTime.new(@beg_year, @beg_month, @beg_day, @beg_hour)
-    @end_date = DateTime.new(@end_year, @end_month, @end_day, @end_hour) - 1.seconds
+    @beg_date = DateTime.new(@beg_year, @beg_month, @beg_day, @beg_hour, @beg_min)
+    @end_date = DateTime.new(@end_year, @end_month, @end_day, @end_hour, @end_min) - 1.seconds
     @date_range = @beg_date..@end_date
   end
 
   def get_date_search_specificity(params)
-    if params[:hour]
+    if params[:min]
+      return :min
+    elsif params[:hour]
       return :hour
     elsif params[:day]
       return :day
@@ -135,6 +152,7 @@ class Appointment < ActiveRecord::Base
   end
 
   def delete_unsearchable_params(appt_params)
+    appt_params.delete(:min) if !appt_params[:min].nil?
     appt_params.delete(:hour) if !appt_params[:hour].nil?
     appt_params.delete(:day) if !appt_params[:day].nil?
     appt_params.delete(:month) if !appt_params[:month].nil?
